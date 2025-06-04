2da1cdba783b71837ea3fa5e172aa165dfb6749d
LostoVayne <moises.barrera.dev@gmail.com>
2025-06-04 03:00:16 -0400

    add input type in FormField component

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,12 +1,62 @@
 import FileInput from "@/components/FileInput";
 import FormField from "@/components/FormField";
+import { ChangeEvent, useState } from "react";
+
+interface FormInitialState {
+  title: string;
+  description: string;
+  visibility: string;
+}
 
 const UploadPage = () => {
+  const [error, setError] = useState<string | null>(null);
+  const [formData, setFormData] = useState<FormInitialState>({
+    title: "",
+    description: "",
+    visibility: "public",
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div className="wrapper-md upload-page">
       <h1>Upload a video</h1>
-      <FormField />
-      <FileInput />
+      {error && <div className="error-field">{error}</div>}
+
+      <form className="rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5">
+        <FormField
+          id="title"
+          label="Title"
+          value={formData.title}
+          onChange={handleInputChange}
+          placeholder="Enter a clear and concise video title"
+        />
+        <FormField
+          id="description"
+          label="Description"
+          value={formData.description}
+          onChange={handleInputChange}
+          placeholder="Describe what this video is about"
+          as="textarea"
+        />
+        <FileInput />
+        <FileInput />
+
+        <FormField
+          id="visibility"
+          label="Visibility"
+          value={formData.visibility}
+          onChange={handleInputChange}
+          as="select"
+          options={[
+            { value: "public", label: "Public" },
+            { value: "private", label: "Private" },
+          ]}
+        />
+      </form>
     </div>
   );
 };
--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,6 +1,28 @@
-const FormField = () => {
+const FormField = ({
+  id,
+  label,
+  type = "text",
+  value,
+  onChange,
+  placeholder,
+  as = "input",
+  options = [],
+}: FormFieldProps) => {
+  const InputToRender = ({ typeInput }: { typeInput: "input" | "textarea" | "select" }) => {
+    if (typeInput === "textarea") {
+      return <textarea />;
+    } else if (typeInput === "select") {
+      return <select />;
+    } else {
+      return <input type={type} />;
+    }
+  };
+
   return (
-    <div>FormField</div>
-  )
-}
-export default FormField
+    <div className="form-field">
+      <label htmlFor={id}>{label}</label>
+      <InputToRender typeInput={as} />
+    </div>
+  );
+};
+export default FormField;
